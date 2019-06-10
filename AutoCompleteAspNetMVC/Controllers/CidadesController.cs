b47650783011bd09a03f3ed947b6bb067bb82ea3
gabrielamnz <gmnzfranco@gmail.com>
2019-06-10 20:47:01 -0300

    Buscap CEP

---
+++ AutoCompleteAspNetMVC/Controllers/CidadesController.cs
@@ -0,0 +1,32 @@
+﻿using AutoCompleteAspNetMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoCompleteAspNetMVC.Controllers
+{
+    public class CidadesController : Controller
+    {
+        // GET: Cidades
+        public JsonResult ListarCidadesPorUF(string uf)
+        {
+            var db = new ClientesContext();
+            var cidades = db.Cidades
+                        .Where(c => c.UF == uf)
+                        //Codigo abaixo necessário para apresentar apenas o que nós queremo, que no caso é ID e NOME
+                        .Select(c => new { Id = c.Id, Nome = c.Nome });
+
+            //A lista de objetos chamada "cidades" é convertida para Json
+            return Json(cidades, JsonRequestBehavior.AllowGet);
+        }
+
+        //    public JsonResult DadosCEP(string cep)
+        //    {
+        //        var ListaCEP = "https://viacep.com.br/ws/" + cep.Replace("-", "") + "/json/";
+
+        //        return Json(ListaCEP,JsonRequestBehavior.AllowGet);
+        //    }
+    }
+}
